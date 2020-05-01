e5eb74dab6d0146fbe07fdc2c767054c04ea7829
Niraj <amatyaraj@gmail.com>
2020-05-01 18:25:33 +1000

    new changes

--- supermarket.js
+++ supermarket.js
@@ -1,39 +1,47 @@
 class Till {
-    constructor() {
-        this.customerTime = [];
-    }
-    totalTime() {
-        // return sum of time required by all the customerTime
-        // return 0 if array is empty
-    }
-    addCustomer(time) {
-        this.customerTime.push(time);
-    }
+	constructor() {
+		this.customerTime = [];
+	}
+	totalTime() {
+		let total = 0;
+		// return sum of time required by all the customerTime
+		// return 0 if array is empty
+		if (this.customerTime === []) {
+			return 0;
+		}
+		for (let i = 0; i <= this.customerTime.length; i++) {
+			total = total + this.customerTime[i];
+		}
+		return total;
+	}
+	addCustomer(time) {
+		this.customerTime.push(time);
+	}
 }
 
 function tillWithSmallestQueue(tills) {
-    // return till with lowest totalTime
+	// return till with lowest totalTime
 }
 
 function tillWithLargestQueue(tills) {
-    // return till with highest totalTime
+	// return till with highest totalTime
 }
 
 function queueTime(customers, n) {
-    // Your code here
-    tills = Array(n).map(t => new Till());
+	// Your code here
+	tills = Array(n).map((t) => new Till());
 
-    // process each customer
-    customers.forEach(customer => {
-        // get till with lowest totalTime
-        let smallestTill = tillWithSmallestQueue(tills);
-        // add customer to that till
-        smallestTill.addCustomer(customer);
-    });
-    
-    // find till with larget totalTime
-    let largestTill = tillWithLargestQueue(tills);
-    // that's the output we need
-    return largestTill.totalTime();
+	// process each customer
+	customers.forEach((customer) => {
+		// get till with lowest totalTime
+		let smallestTill = tillWithSmallestQueue(tills);
+		// add customer to that till
+		smallestTill.addCustomer(customer);
+	});
+
+	// find till with larget totalTime
+	let largestTill = tillWithLargestQueue(tills);
+	// that's the output we need
+	return largestTill.totalTime();
 }
-module.exports = {queueTime}
+module.exports = { queueTime };
